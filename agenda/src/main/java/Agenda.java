188dd8b6e7d2bb938c2ea913934c0175a3638de2
matheus.ssouza1 <matheus.ssouza1@CAS419173W0720.senacsp.edu.br>
2016-02-26 22:18:15 -0300

    bando de dados  atualizado e dependencias do jdbc

--- agenda/src/main/java/Agenda.java
+++ agenda/src/main/java/Agenda.java
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.senac.tads.pi3.mssouza00.agenda;
 
 /**
  *
  * @author matheus.ssouza1
  */
 public class Agenda {
-    int var;
+    
 }
